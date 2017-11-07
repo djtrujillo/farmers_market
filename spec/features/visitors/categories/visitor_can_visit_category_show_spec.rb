bf00fd4ef78a2257fb0b24ed9847d00a527b030a
djtrujillo <danny.j.trujillo@gmail.com>
2017-11-06 19:35:43 -0700

    added test for and function for category show page

---
+++ spec/features/visitors/categories/visitor_can_visit_category_show_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+feature "visitor can visit category show page" do
+  scenario "from category index" do
+
+    category = create(:category)
+
+    item = create(:item, category_id: category.id)
+    visit "/categories"
+
+    click_on "#{category.title}"
+
+    expect(current_path).to eq("/categories/#{category.id}")
+
+    expect(page).to have_content(category.title)
+    expect(page).to have_content(item.name)
+    expect(page).to have_content(item.price)
+    expect(page).to have_content(item.unit_measurement)
+    expect(page).to have_link("Add to Basket")
+  end
+end

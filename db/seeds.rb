c2505af9214af311138516541d4e3bae62dce870
djtrujillo <danny.j.trujillo@gmail.com>
2017-11-08 11:36:22 -0700

    trying to get seeds to run in heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Item.destroy_all
-# Category.destroy_all
-# User.destroy_all
-# Basket.destroy_all
 
 
 lettuce = Category.create(title: "Lettuce")
